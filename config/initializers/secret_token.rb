d3327524666edb41a845f8101e14a75ab8a8da11
Alex Kroman <alex@Alex-Kromans-MacBook-Pro.local>
2011-05-06 14:41:16 -0700

    New site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catering::Application.config.secret_token = 'f4eb9d71c7454f9fdee26a696550a08a38e18b6aee80d24ab984975724000415b3414b5107c49094763f42a8ae4ce411a6d71ae7a008be59e2cf36d1f9edd173'
